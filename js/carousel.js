0e58c1eefcef876b2611388b9b4e5f5cc0432439
eg-delacruz <netoxas3107@hotmail.com>
2021-04-03 13:36:09 +0200

    Mejorado carousel con librería glide.js. Mejorada calidad de algunas imágenes. Algunos cambios en paddings. Creado html para página en construcción y listos sus estilos para crear los elementos.

--- js/carousel.js
+++ js/carousel.js
@@ -1,36 +1,18 @@
-window.addEventListener("load", function () {
-  new Glider(document.querySelector(".carousel__list"), {
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    duration: 0.8,
-    dots: ".carousel__indicators",
-    draggable: true,
-    arrows: {
-      prev: ".carousel__back",
-      next: ".carousel__next",
+const config = {
+  type: "carousel",
+  perView: 4,
+  startAt: 0,
+  autoplay: 2000,
+  breakpoints: {
+    767: {
+      perView: 3,
     },
-    responsive: [
-      {
-        // screens greater than >= 775px
-        breakpoint: 450,
-        settings: {
-          // Set to `auto` and provide item width to adjust to viewport
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          // itemWidth: 150,
-          // duration: 0.25,
-        },
-      },
-      {
-        // screens greater than >= 1024px
-        breakpoint: 767,
-        settings: {
-          slidesToShow: 4,
-          slidesToScroll: 4,
-          itemWidth: 150,
-          duration: 0.25,
-        },
-      },
-    ],
-  });
-});
+    625: {
+      perView: 2,
+    },
+    410: {
+      perView: 1,
+    },
+  },
+};
+new Glide(".glide", config).mount();
