87362730df499b9246428667661218018f23c16c
eg-delacruz <netoxas3107@hotmail.com>
2021-04-20 17:53:34 +0200

    Añadido craco.config.js para hacer scoped scss

--- campuscanvas/craco.config.js
+++ campuscanvas/craco.config.js
@@ -1,2 +1,10 @@
 // Aquí va la config para usar craco, que ayuda a
 // hacer que los .scss sean scoped
+
+module.exports = {
+  plugins: [
+    {
+      plugin: require('craco-plugin-scoped-css'),
+    },
+  ],
+};
