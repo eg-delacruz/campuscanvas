bf5d3b7eaf9130f947128547d8bb68208bd8909d
eg-delacruz <netoxas3107@hotmail.com>
2021-10-17 23:08:50 +0200

    Terminado componente de formulario de contacto completamente funcional. Se reciben los mensajes en el correo de CC

--- campuscanvas/src/components/ContactForm/ContactForm.jsx
+++ campuscanvas/src/components/ContactForm/ContactForm.jsx
@@ -15,49 +15,84 @@ const ContactForm = () => {
   const MESSAGE = useInputValue('');
 
   const handleSubmit = (e) => {
-    e.preventDefault();
+    //e.preventDefault();
   };
 
   return (
-    <form className='form' onSubmit={handleSubmit} action=''>
+    <form
+      className='form'
+      method='POST'
+      onSubmit={handleSubmit}
+      action='https://formsubmit.co/a6c2cdb34bab8fc6bd2a306139ff5fdb'
+    >
       <h3 className='form__title'>Formulario de contacto</h3>
-      <input
-        className='form__input'
-        type='text'
-        placeholder='Nombre *'
-        {...NAME}
-      />
-      <input
-        className='form__input'
-        type='text'
-        placeholder='Apellido *'
-        {...LAST_NAME}
-      />
-      <input
-        className='form__input'
-        type='email'
-        placeholder='Correo empresarial *'
-        {...EMAIL}
-      />
-      <input
-        className='form__input'
-        type='text'
-        placeholder='Empresa'
-        {...COMPANY}
-      />
-      <input
-        className='form__input'
-        type='tel'
-        placeholder='Número de teléfono'
-        {...PHONE}
-      />
+
+      <label className='form__labelTag'>
+        <input
+          className='form__input'
+          type='text'
+          placeholder='nombre'
+          required
+          name='nombre'
+          {...NAME}
+        />
+        <span className='form__inputLabel'>Nombre *</span>
+      </label>
+
+      <label className='form__labelTag'>
+        <input
+          className='form__input'
+          type='text'
+          placeholder='Apellido *'
+          required
+          name='Apellido'
+          {...LAST_NAME}
+        />
+        <span className='form__inputLabel'>Apellido *</span>
+      </label>
+
+      <label className='form__labelTag'>
+        <input
+          className='form__input'
+          type='email'
+          placeholder='Correo empresarial *'
+          name='email'
+          required
+          {...EMAIL}
+        />
+        <span className='form__inputLabel'>Correo empresarial *</span>
+      </label>
+
+      <label className='form__labelTag'>
+        <input
+          className='form__input'
+          type='text'
+          placeholder='Empresa'
+          name='empresa'
+          {...COMPANY}
+        />
+        <span className='form__inputLabel'>Empresa</span>
+      </label>
+
+      <label className='form__labelTag'>
+        <input
+          className='form__input'
+          type='tel'
+          placeholder='Número de teléfono'
+          name='telefono'
+          {...PHONE}
+        />
+        <span className='form__inputLabel'>Teléfono</span>
+      </label>
+
       <textarea
         className='form__input form__message'
         name='user_message'
-        placeholder='Tus preguntas aquí'
+        placeholder='Escribe aquí tu mensaje'
+        required
         {...MESSAGE}
       />
-      <button className='form__button' type='submit'>
+      <button className='button-blue btn form__button' type='submit'>
         Enviar
       </button>
     </form>
