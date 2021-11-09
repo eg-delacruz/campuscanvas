6ddebece809a68edf1402af4f46d516a458c65e7
eg-delacruz <netoxas3107@hotmail.com>
2021-11-09 21:37:49 +0100

    Terminada página principal de blog con todos sus MQ.

--- campuscanvas/src/components/BlogTable/PostsTable.jsx
+++ campuscanvas/src/components/BlogTable/PostsTable.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 //Styles
 import './PostsTable.scoped.scss';
@@ -19,8 +20,6 @@ const PostsTable = () => {
       Title: 'Título del post',
       Image: ImagePost1,
       Author: 'Gerardo De La Cruz',
-      CoverIntro:
-        "'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'",
 
       //Separate each paragraph with a comma in the array
       Content: [
@@ -35,8 +34,6 @@ const PostsTable = () => {
       Title: 'Título del post',
       Image: ImagePost2,
       Author: 'Gerardo De La Cruz',
-      CoverIntro:
-        "'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'",
 
       //Separate each paragraph with a comma in the array
       Content: [
@@ -51,8 +48,6 @@ const PostsTable = () => {
       Title: 'Título del post',
       Image: ImagePost3,
       Author: 'Gerardo De La Cruz',
-      CoverIntro:
-        "'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'",
 
       //Separate each paragraph with a comma in the array
       Content: [
@@ -67,8 +62,6 @@ const PostsTable = () => {
       Title: 'Título del post',
       Image: ImagePost4,
       Author: 'Gerardo De La Cruz',
-      CoverIntro:
-        "'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'",
 
       //Separate each paragraph with a comma in the array
       Content: [
@@ -83,8 +76,6 @@ const PostsTable = () => {
       Title: 'Título del post',
       Image: ImagePost5,
       Author: 'Gerardo De La Cruz',
-      CoverIntro:
-        "'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'",
 
       //Separate each paragraph with a comma in the array
       Content: [
@@ -99,8 +90,6 @@ const PostsTable = () => {
       Title: 'Título del post',
       Image: ImagePost6,
       Author: 'Gerardo De La Cruz',
-      CoverIntro:
-        "'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'",
 
       //Separate each paragraph with a comma in the array
       Content: [
@@ -116,8 +105,6 @@ const PostsTable = () => {
       Title: 'Título del post',
       Image: ImagePost7,
       Author: 'Gerardo De La Cruz',
-      CoverIntro:
-        "'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'",
 
       //Separate each paragraph with a comma in the array
       Content: [
@@ -131,6 +118,25 @@ const PostsTable = () => {
   //Main post = last post in array
   const MAIN_POST = [POSTS[POSTS.length - 1]];
 
+  //All posts without main post
+  const ALL_POSTS = createAllPostsArray(POSTS);
+  function createAllPostsArray(posts_array) {
+    const ALL_POSTS = [...posts_array];
+    ALL_POSTS.pop();
+    return ALL_POSTS;
+  }
+
+  //Text shortener function
+  function truncateText(text, maxLength) {
+    let shortenedText;
+    if (text.length > maxLength) {
+      shortenedText = text.substr(0, maxLength) + '...';
+    } else {
+      shortenedText = text;
+    }
+    return shortenedText;
+  }
+
   return (
     <div className='Posts'>
       {/*   /////////////////////////
@@ -143,12 +149,16 @@ const PostsTable = () => {
           {MAIN_POST.map((post) => (
             <div key={post.id} className='MainPost'>
               <figure className='MainPost__image'>
-                <img src={post.Image} alt='' />
+                <img src={post.Image} alt='Imagen de la publicación' />
               </figure>
-              <div className='MainPost__description'>
-                <h1>{post.Title}</h1>
-                <p>{post.CoverIntro}</p>
-              </div>
+              <h1>{post.Title}</h1>
+              <p>{truncateText(post.Content[0], 350)}</p>
+              <Link
+                to={`blog/post/${post.id}`}
+                className='btn button--redRedborderTransparent'
+              >
+                Leer más
+              </Link>
             </div>
           ))}
         </div>
@@ -157,7 +167,31 @@ const PostsTable = () => {
       {/*   /////////////////////////
             //     Posts Table     //
             ///////////////////////// */}
-      <section>Ultimos posts</section>
+      <section className='AllPosts container'>
+        <h3>Últimos Posts</h3>
+        <hr />
+
+        <div className='AllPostsGrid'>
+          {ALL_POSTS.map((post) => (
+            <div key={post.id} className='post'>
+              <figure>
+                <img src={post.Image} alt='Portada del post' />
+              </figure>
+              <h4>{post.Title}</h4>
+              <p className='post__description'>
+                {truncateText(post.Content[0], 170)}
+              </p>
+              <Link
+                to={`blog/post/${post.id}`}
+                className='btn button--redRedborderTransparent'
+              >
+                Leer más
+              </Link>
+              <hr />
+            </div>
+          ))}
+        </div>
+      </section>
     </div>
   );
 };
