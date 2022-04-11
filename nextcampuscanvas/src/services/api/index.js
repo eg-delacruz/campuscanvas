c37456d93bbee138e505fbeaac24113ecedd47ba
eg-delacruz <netoxas3107@hotmail.com>
2022-04-11 11:51:10 +0200

    Modificando nombres de variables de entorno

--- nextcampuscanvas/src/services/api/index.js
+++ nextcampuscanvas/src/services/api/index.js
@@ -1,15 +1,10 @@
 //Aquí manejaremos todos los puntos de entrada de la API desde el FRONT!!
 
-//const API = process.env.NEXT_PUBLIC_API_URL;
-//const VERSION = process.env.NEXT_PUBLIC_API_VERSION;
-
-const API = process.env.NEXT_PUBLIC_API_URL_CC;
-const VERSION = process.env.NEXT_PUBLIC_API_URL_CC_VERSION;
+const API = process.env.NEXT_PUBLIC_API_URL;
+const VERSION = process.env.NEXT_PUBLIC_API_URL_VERSION;
 
 const endPoints = {
   auth: {
-    //TODO: borrar ruta de prueba
-    //login: `${API}/api/${VERSION}/auth/login`,
     login: `${API}/api/auth/signup`,
   },
   user: {
