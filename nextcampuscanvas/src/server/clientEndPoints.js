cb767f89ff703d760a75c27a091d108d8e447e39
eg-delacruz <netoxas3107@hotmail.com>
2022-04-22 23:44:15 +0200

    Terminado sistema que maneja estados de UI según estado de cargando, usuario registrad, usuario verificado y sin sesión

--- nextcampuscanvas/src/server/clientEndPoints.js
+++ nextcampuscanvas/src/server/clientEndPoints.js
@@ -4,7 +4,7 @@ const PRODUCTION_API = process.env.NEXT_PUBLIC_API_URL;
 const VERSION = process.env.NEXT_PUBLIC_API_URL_VERSION;
 
 //change this to production URL when uploading to server
-const API = LOCAL_API;
+const API = PRODUCTION_API;
 
 const endPoints = {
   user: {
