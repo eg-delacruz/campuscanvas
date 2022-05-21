c715d01faa7dafe33b9d3163685288c7c40e7f49
eg-delacruz <netoxas3107@hotmail.com>
2022-05-21 19:00:34 +0200

    Test1

--- nextcampuscanvas/src/services/api/index.js
+++ nextcampuscanvas/src/services/api/index.js
@@ -6,7 +6,7 @@ const PROD_API = process.env.NEXT_PUBLIC_API_URL;
 const VERSION = process.env.NEXT_PUBLIC_API_URL_VERSION;
 
 //Befor sending to production and npm build, change the API URL to the production one
-const API = LOCAL_API;
+const API = PROD_API;
 
 const endPoints = {
   auth: {
