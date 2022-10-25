61e00b64552709a44a829f86a93da05fe1fcadeb
eg-delacruz <netoxas3107@hotmail.com>
2022-10-25 17:49:15 +0200

    Testing

--- nextcampuscanvas/src/pages/api/v1/file_management/student_acc_files/stu_id_files.js
+++ nextcampuscanvas/src/pages/api/v1/file_management/student_acc_files/stu_id_files.js
@@ -17,6 +17,7 @@ import NextCors from 'nextjs-cors';
 //TODO: upload this route to next.js to test it before uploading things
 //TODO: continue with help of this blog: https://betterprogramming.pub/upload-files-to-next-js-with-api-routes-839ce9f28430
 //TODO: continue with help of https://www.youtube.com/watch?v=jwp4U6v-3h4&list=WL&index=6&t=484s
+//Testing
 const router = createRouter();
 
 router
