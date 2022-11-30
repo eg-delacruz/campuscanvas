4074686d2bc48fd8ca4a5f804a9c8ed7c8f1aee8
eg-delacruz <netoxas3107@hotmail.com>
2022-11-30 19:08:00 +0100

    Stars of home box animated

--- nextcampuscanvas/src/components/GeneralUseComponents/AnimatedBox/AnimatedBox.jsx
+++ nextcampuscanvas/src/components/GeneralUseComponents/AnimatedBox/AnimatedBox.jsx
@@ -7,6 +7,7 @@ import styles from './AnimatedBox.module.scss';
 import box_back from '@assets/GeneralUse/AnimatedBoxElements/box_back.svg';
 import products from '@assets/GeneralUse/AnimatedBoxElements/products.svg';
 import box_front from '@assets/GeneralUse/AnimatedBoxElements/box_front.svg';
+import star from '@assets/GeneralUse/AnimatedBoxElements/star.svg';
 
 //Components
 
@@ -17,6 +18,88 @@ const AnimatedBox = () => {
       <div className={styles.box_back}>
         <img src={box_back.src} />
       </div>
+
+      {/* /////////////////////////
+        //         Stars         //
+        ///////////////////////// */}
+      <div className={styles.stars_container}>
+        <div className={`${styles.star_small} ${styles.star1}`}>
+          <img src={star.src} />
+        </div>
+        <div className={`${styles.star_small} ${styles.star2}`}>
+          <img src={star.src} />
+        </div>
+        <div className={`${styles.star_small} ${styles.star3}`}>
+          <img src={star.src} />
+        </div>
+        <div className={`${styles.star_small} ${styles.star4}`}>
+          <img src={star.src} />
+        </div>
+        <div className={`${styles.star_small} ${styles.star5}`}>
+          <img src={star.src} />
+        </div>
+        <div className={`${styles.star_small} ${styles.star6}`}>
+          <img src={star.src} />
+        </div>
+        <div className={`${styles.star_small} ${styles.star7}`}>
+          <img src={star.src} />
+        </div>
+        <div className={`${styles.star_small} ${styles.star8}`}>
+          <img src={star.src} />
+        </div>
+        <div className={`${styles.star_big} ${styles.star9}`}>
+          <img src={star.src} />
+        </div>
+        <div className={`${styles.star_big} ${styles.star10}`}>
+          <img src={star.src} />
+        </div>
+        <div className={`${styles.star_big} ${styles.star11}`}>
+          <img src={star.src} />
+        </div>
+        <div className={`${styles.star_big} ${styles.star12}`}>
+          <img src={star.src} />
+        </div>
+        <div className={`${styles.star_big} ${styles.star13}`}>
+          <img src={star.src} />
+        </div>
+        <div className={`${styles.star_big} ${styles.star14}`}>
+          <img src={star.src} />
+        </div>
+        <div className={`${styles.star_big} ${styles.star15}`}>
+          <img src={star.src} />
+        </div>
+        <div className={`${styles.star_big} ${styles.star16}`}>
+          <img src={star.src} />
+        </div>
+        <div className={`${styles.star_small} ${styles.star17}`}>
+          <img src={star.src} />
+        </div>
+        <div className={`${styles.star_big} ${styles.star18}`}>
+          <img src={star.src} />
+        </div>
+        <div className={`${styles.star_small} ${styles.star19}`}>
+          <img src={star.src} />
+        </div>
+        <div className={`${styles.star_big} ${styles.star20}`}>
+          <img src={star.src} />
+        </div>
+        <div className={`${styles.star_small} ${styles.star21}`}>
+          <img src={star.src} />
+        </div>
+        <div className={`${styles.star_big} ${styles.star22}`}>
+          <img src={star.src} />
+        </div>
+        <div className={`${styles.star_small} ${styles.star23}`}>
+          <img src={star.src} />
+        </div>
+        <div className={`${styles.star_big} ${styles.star24}`}>
+          <img src={star.src} />
+        </div>
+      </div>
+
+      {/* /////////////////////////
+        //  Products and front  //
+        ///////////////////////// */}
       <div className={styles.products}>
         <img src={products.src} />
       </div>
