abf9e5806950819606ab21e803aa25644d1f5ef0
eg-delacruz <netoxas3107@hotmail.com>
2023-01-12 12:32:23 +0100

    useSecureUnverifRouteOnMount hook created

---
+++ nextcampuscanvas/src/hooks/useSecureUnverifRouteOnMount.js
@@ -0,0 +1,37 @@
+import { useEffect, useState } from 'react';
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/router';
+
+const useSecureUnverifRouteOnMount = () => {
+  const [verifyingSession, setVerifyingSession] = useState(true);
+  const { data: session, status } = useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    console.log('session', session);
+    console.log('status', status);
+    if (status === 'unauthenticated') {
+      return router.push({ pathname: 'auth/login' }, 'auth/login');
+    }
+
+    if (session) {
+      if (!session?.token.stu_data.university && !session?.token.stu_verified) {
+        return router.push(
+          { pathname: 'auth/registro', query: { step: 2 } },
+          'auth/registro'
+        );
+      }
+      if (session?.token.stu_data.university && !session?.token.stu_verified) {
+        return router.push(
+          { pathname: 'auth/registro', query: { step: 3 } },
+          'auth/registro'
+        );
+      }
+    }
+    setVerifyingSession(false);
+  }, [session, status]);
+
+  return { verifyingSession };
+};
+
+export default useSecureUnverifRouteOnMount;
--- nextcampuscanvas/src/pages/student/ofertas/[id].jsx
+++ nextcampuscanvas/src/pages/student/ofertas/[id].jsx
@@ -1,5 +1,4 @@
 import { useRouter } from 'next/router';
-import { useSession } from 'next-auth/react';
 
 //Styles
 import styles from '@styles/pagestyles/student/VerifDiscountDisplayer.module.scss';
@@ -10,35 +9,18 @@ import { OFFERS } from '@databases/offers/offersInfoDatabase.js';
 //Components
 import Layout from '@components/GeneralUseComponents/Layout/Layout';
 
+//Hooks
+import useSecureUnverifRouteOnMount from '@hooks/useSecureUnverifRouteOnMount';
+
 const VerifDiscountDisplayer = () => {
+  const { verifyingSession } = useSecureUnverifRouteOnMount();
+
   // //Get offer id
   const router = useRouter();
 
-  //Session
-  const { data: session, status } = useSession();
-
-  //Redirecting according to auth status/securing route
-  if (status === 'unauthenticated') {
-    router.push('/auth/login');
-  }
-  if (session) {
-    if (!session?.token.stu_data.university && !session?.token.stu_verified) {
-      router.push(
-        { pathname: '/auth/registro', query: { step: 2 } },
-        '/auth/registro'
-      );
-    }
-    if (session?.token.stu_data.university && !session?.token.stu_verified) {
-      router.push(
-        { pathname: '/auth/registro', query: { step: 3 } },
-        '/auth/registro'
-      );
-    }
-  }
-
   const id = Number(router.query.id);
 
-  if (status === 'loading' || !session?.token.stu_verified) {
+  if (verifyingSession) {
     return <div>Loading...</div>;
   }
   return <div>VerifDiscountDisplayer</div>;
