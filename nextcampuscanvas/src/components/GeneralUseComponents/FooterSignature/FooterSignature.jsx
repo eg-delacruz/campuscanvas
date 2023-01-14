76c4c6a238c4db292817a2e19399483785311ba6
eg-delacruz <netoxas3107@hotmail.com>
2023-01-14 17:40:38 +0100

    Automatically display current year in footer signature

--- nextcampuscanvas/src/components/GeneralUseComponents/FooterSignature/FooterSignature.jsx
+++ nextcampuscanvas/src/components/GeneralUseComponents/FooterSignature/FooterSignature.jsx
@@ -1,8 +1,11 @@
 import styles from './Footer_signature.module.scss';
 export default function () {
+  //Get current year
+  const YEAR = new Date().getFullYear();
+
   return (
     <section className={styles.footer__signature}>
-      <p>®Campus Canvas 2022. Todos los derechos reservados</p>
+      <p>®Campus Canvas {YEAR}. Todos los derechos reservados</p>
       <p>Dedicado a los estudiantes desde Madrid ❤️</p>
     </section>
   );
