1dfdb8211d831c898a21c7bc07d32d12126a34c9
eg-delacruz <netoxas3107@hotmail.com>
2023-03-16 18:34:15 +0100

    Several bugs fixed. Sending to production to test them

--- nextcampuscanvas/src/pages/_document.js
+++ nextcampuscanvas/src/pages/_document.js
@@ -93,7 +93,7 @@ export default function Document() {
         <meta name='robots' content='index' follow='true' />
 
         {/* Cookie consent banner and widget */}
-        {/* <script
+        <script
           id='Cookiebot'
           src='https://consent.cookiebot.com/uc.js'
           data-cbid='56697194-dfde-4726-ae75-dd1721d25c14'
@@ -102,7 +102,7 @@ export default function Document() {
           onError={() => {
             console.error('Script failed to load');
           }}
-        /> */}
+        />
 
         {/* Facebook Pixel */}
         <meta
