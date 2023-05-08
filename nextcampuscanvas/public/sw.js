edee40d5389a8e5e7a2293a9a56dba3e4d286439
eg-delacruz <netoxas3107@hotmail.com>
2023-05-08 14:54:09 +0200

    Testing new pwa implementation in production

--- nextcampuscanvas/public/sw.js
+++ nextcampuscanvas/public/sw.js
@@ -1,3 +1,13 @@
 self.addEventListener('install', function (event) {
   console.log('Hello world from the Service Worker 🤙');
 });
+
+// activate event
+self.addEventListener('activate', function (event) {
+  console.log('Service Worker has been activated 🤙');
+});
+
+// fetch event
+self.addEventListener('fetch', function (event) {
+  console.log('fetch event' + event);
+});
