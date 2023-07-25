dfa1e2bcdbcea254a0bbb3f027147e24b1c66904
Gerardo De La Cruz <netoxas3107@hotmail.com>
2023-07-25 18:01:48 +0200

    Now, the unhandledstuemails controller checks if the student email entry already exists to avoid storing it multiple times. Before, if the user tried to register without success, the entry would get stored as many times as the user tries

--- nextcampuscanvas/src/server/components/unhandledEmails/store.js
+++ nextcampuscanvas/src/server/components/unhandledEmails/store.js
@@ -1,9 +1,9 @@
-import dbConnect from '@server/dbConnect';
-import config from '@server/config';
+import dbConnect from "@server/dbConnect";
+import config from "@server/config";
 dbConnect(config.dbURL);
 
 //Model
-import UnhandledEmailEntry from '@server/components/unhandledEmails/model';
+import UnhandledEmailEntry from "@server/components/unhandledEmails/model";
 
 /////////////////////Add entry//////////////////////////////
 
@@ -16,7 +16,13 @@ const getAll = async () => {
   return await UnhandledEmailEntry.find();
 };
 
+/////////////////////Get by email//////////////////////////////
+const getByEmail = async (stu_email) => {
+  return await UnhandledEmailEntry.findOne({ stu_email });
+};
+
 module.exports = {
   add: addunhandledEmailEntry,
   getAll,
+  getByEmail,
 };
